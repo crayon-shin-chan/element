de1df7a43b10fe8d7c91dcdb121bb27e46528110
chenzhenyuan@skyable.cn <1253665625@qq.com>
2020-08-11 12:28:42 +0800

    主题色更改

--- examples/components/theme-picker.vue
+++ examples/components/theme-picker.vue
@@ -21,31 +21,39 @@
   }
 </style>
 
+/** 主题选择器组件 */
 <script>
   import Element from 'main/index.js';
-  const { version } = Element;
+  const { version } = Element;//element-ui版本
 
-  const ORIGINAL_THEME = '#409EFF';
+  const ORIGINAL_THEME = '#409EFF';//默认主题
   export default {
     data() {
       return {
-        chalk: '', // content of theme-chalk css
+        chalk: '', // css字符串
         docs: '', // content of docs css
-        theme: ORIGINAL_THEME
+        theme: ORIGINAL_THEME//主题色
       };
     },
 
     watch: {
+      /** 主题色更改 */  
       theme(val, oldVal) {
         if (typeof val !== 'string') return;
+        /** 主题颜色簇，不同深浅 */
         const themeCluster = this.getThemeCluster(val.replace('#', ''));
+        /** 原颜色簇 */
         const originalCluster = this.getThemeCluster(oldVal.replace('#', ''));
+        /** 获取处理器，更改指定id元素，指定变量的内容为指定css字符串 */
         const getHandler = (variable, id) => {
           return () => {
+            /** 默认主题颜色簇 */  
             const originalCluster = this.getThemeCluster(ORIGINAL_THEME.replace('#', ''));
+            /** 更新默认主题色为新选择的主题色，获取css字符串 */
             let newStyle = this.updateStyle(this[variable], originalCluster, themeCluster);
-
+            /** style元素 */
             let styleTag = document.getElementById(id);
+            /** 不存在则创建 */
             if (!styleTag) {
               styleTag = document.createElement('style');
               styleTag.setAttribute('id', id);
@@ -54,10 +62,11 @@
             styleTag.innerText = newStyle;
           };
         };
-
+        /** 更改chalk变量，style元素id为chalk-style */
         const chalkHandler = getHandler('chalk', 'chalk-style');
         const docsHandler = getHandler('docs', 'docs-style');
 
+        /** chalk不存在，远程获取css字符串 */
         if (!this.chalk) {
           const url = `https://unpkg.com/element-ui@${ version }/lib/theme-chalk/index.css`;
           this.getCSSString(url, chalkHandler, 'chalk');
@@ -65,20 +74,25 @@
           chalkHandler();
         }
 
+        /** docs不存在 */
         if (!this.docs) {
+            /** 获取所有href包含docs.css的link元素 */
           const links = [].filter.call(document.querySelectorAll('link'), link => {
             return /docs\..+\.css/.test(link.href || '');
           });
+          /** 获取link指定css内容存储到docs */
           links[0] && this.getCSSString(links[0].href, docsHandler, 'docs');
         } else {
           docsHandler();
         }
 
+        /** 获取所有style元素，过滤出包含oldVal颜色以及包含Chalk Variables的 */
         const styles = [].slice.call(document.querySelectorAll('style'))
           .filter(style => {
             const text = style.innerText;
             return new RegExp(oldVal, 'i').test(text) && !/Chalk Variables/.test(text);
           });
+        /** 替换style内部css字符串 */  
         styles.forEach(style => {
           const { innerText } = style;
           if (typeof innerText !== 'string') return;
@@ -108,15 +122,20 @@
         xhr.send();
       },
 
+      /** 获取指定主题色的颜色簇 */
       getThemeCluster(theme) {
+
+        /** 染色函数，获取深色，tint为染色程度，0.0-0.9，用于等分剩下的颜色值，如果原来的红色值为100，则将100-255分为10份 */  
         const tintColor = (color, tint) => {
+          /** 获取红绿蓝三色整数值 */  
           let red = parseInt(color.slice(0, 2), 16);
           let green = parseInt(color.slice(2, 4), 16);
           let blue = parseInt(color.slice(4, 6), 16);
-
+          /** 0即不变 */
           if (tint === 0) { // when primary color is in its rgb space
             return [red, green, blue].join(',');
           } else {
+            /** 加深颜色 */  
             red += Math.round(tint * (255 - red));
             green += Math.round(tint * (255 - green));
             blue += Math.round(tint * (255 - blue));
@@ -128,7 +147,7 @@
             return `#${ red }${ green }${ blue }`;
           }
         };
-
+        /** 获取浅色，原颜色值分为10份，获取不同程度浅色 */
         const shadeColor = (color, shade) => {
           let red = parseInt(color.slice(0, 2), 16);
           let green = parseInt(color.slice(2, 4), 16);
@@ -145,10 +164,14 @@
           return `#${ red }${ green }${ blue }`;
         };
 
+        /** 默认颜色为原主题色 */
         const clusters = [theme];
+        
         for (let i = 0; i <= 9; i++) {
+          /** 0-0.9等9个较深颜色 */  
           clusters.push(tintColor(theme, Number((i / 10).toFixed(2))));
         }
+        /** 获取一个浅色，为原来色值的0.9倍 */
         clusters.push(shadeColor(theme, 0.1));
         return clusters;
       }
--- examples/components/theme/loader/docStyle.vue
+++ examples/components/theme/loader/docStyle.vue
@@ -6,15 +6,16 @@ import { updateDomHeadStyle } from '../utils.js';
 export default {
   data() {
     return {
-      docs: '', // content of docs css
-      theme: ORIGINAL_THEME,
-      asyncCb: true
+      docs: '', // css字符串
+      theme: ORIGINAL_THEME,//默认主题色
+      asyncCb: true//异步回调
     };
   },
   methods: {
     updateDocStyle(e, cb) {
-      const val = e.global['$--color-primary'] || ORIGINAL_THEME;
-      const oldVal = this.theme;
+      const val = e.global['$--color-primary'] || ORIGINAL_THEME;//获取新主题色
+      const oldVal = this.theme;//原主题色
+      /** 处理器更新指定dom元素的内容为新的css字符串，并且更新指定变量 */
       const getHandler = (variable, id) => {
         return () => {
           let newStyle = this.updateStyle(this[variable], ORIGINAL_THEME, val);
@@ -22,19 +23,24 @@ export default {
           this.asyncCb && cb();
         };
       };
+      /** 更新docs变量、id为docs-style的元素的处理器 */
       const docsHandler = getHandler('docs', 'docs-style');
+      /** 当前不存在更新后css字符串 */
       if (!this.docs) {
+        /** 获取href包含docs.css的link元素列表 */  
         const links = [].filter.call(document.querySelectorAll('link'), link => {
           return /docs\..+\.css/.test(link.href || '');
         });
+        /** 获取css字符串存储在docs中 */
         if (links[0]) {
           this.getCSSString(links[0].href, docsHandler, 'docs');
         } else {
           this.asyncCb = false;
         }
       } else {
-        docsHandler();
+        docsHandler();//更新css字符串
       }
+      /** 获取所有内容包含oldVal以及!/Chalk Variables/的style元素 */
       const styles = [].slice.call(document.querySelectorAll('style'))
         .filter(style => {
           const text = style.innerText;
@@ -43,14 +49,18 @@ export default {
       styles.forEach(style => {
         const { innerText } = style;
         if (typeof innerText !== 'string') return;
+        /** 更新style元素内部css字符串 */
         style.innerText = this.updateStyle(innerText, oldVal, val);
       });
-      this.theme = val;
+      this.theme = val;//新的主题
       !this.asyncCb && cb();
     },
+    /** 更新css字符串文本，返回替换后新的文本 */
     updateStyle(style, oldColor, newColor) {
+      /** 替换指定颜色 */  
       return style.replace(new RegExp(oldColor, 'ig'), newColor);
     },
+    /** 获取指定url的css字符串，存储到变量中 */
     getCSSString(url, callback, variable) {
       ajaxGet(url).then((res) => {
         this[variable] = res;
